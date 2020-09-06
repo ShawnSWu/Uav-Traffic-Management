59409b177275d6db3da333f1fa0ac461e56c171e
shawnwu4mac <shawnishere0806>
2020-09-07 03:02:08 +0800

    :tada:  init commit

---
+++ src/main/java/com/nutn/utm/UtmApplication.java
@@ -0,0 +1,13 @@
+package com.nutn.utm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutn/utm/UtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutn.utm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
